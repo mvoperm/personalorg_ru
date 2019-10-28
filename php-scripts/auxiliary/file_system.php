69c7479e4b19afb2dbc7c2a436004533bc32773a
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2019-10-28 18:17:30 +0300

    Корректировка файла получения контента Пользователя в связи с ненадобностью файла XML для страницы настроек

--- php-scripts/auxiliary/file_system.php
+++ php-scripts/auxiliary/file_system.php
@@ -13,16 +13,12 @@ function create_user_dir($user_folder)	{
 
 // Открытие или создание файла content пользователя
 function get_xml_content($user_folder, $content)	{
-	if ($content == 'options')	{
-		$result_file = USERS_DIR . 'optionspage.xml';
-	} else	{
 		$result_file = USERS_DIR . $user_folder . '/' . $content . '.xml';
 		if (file_exists($result_file) === false) {
 			$template_file = USERS_DIR . 'content_template.xml';
 			$success = copy($template_file, $result_file);
 			if ($success === false)	{$result_file = '';}
 		}
-	}
 	return $result_file;
 }
 
