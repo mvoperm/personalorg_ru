b9d2a6a10d6fb684dcc935831b9b24cdbdb9703c
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2019-11-19 12:14:55 +0300

    Косметические правки в файле html_content_classes.php

--- public_html/html-code/html_content_classes.php
+++ public_html/html-code/html_content_classes.php
@@ -4,8 +4,8 @@ class Folder {
   public $title = '';
   public $parent_id = '';
   public $local_id = '';
-  public $folders = array();
-  public $items = array();
+  public $folders = [];
+  public $items = [];
   public function get_total_id() {
     $total_id = ($this -> local_id === '0' || $this -> parent_id === '0') ? $this -> local_id : $this -> parent_id . '-' . $this -> local_id ;
     return $total_id;
@@ -20,7 +20,7 @@ class Item {
   public $local_id = '';
   public $title = '';
   public $uri = '';
-  public $text = array();
+  public $text = [];
 }
 
 $options_code = [
