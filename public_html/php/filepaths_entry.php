dc3404d21c50396d83a12da347dbbe09e473ee92
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2019-11-26 16:11:48 +0300

    Переименование папки с php-файлами, общими для всех блоков

--- public_html/php/filepaths_entry.php
+++ public_html/php/filepaths_entry.php
@@ -5,7 +5,7 @@
 // Адрес корневых каталогов
 define('DOMAIN_ROOT', $_SERVER['DOCUMENT_ROOT']); // Корневой каталог домена для целей require_once ('/home/x/x926842w/x926842w.beget.tech/public_html')
 define('PHP_FOLDER', '/php'); // Корневой каталог домена для целей require_once ('/home/x/x926842w/x926842w.beget.tech/public_html')
-require_once(DOMAIN_ROOT . PHP_FOLDER . '/auxiliary/domain_paths.php'); // Константы наименований путей доменных имён
+require_once(DOMAIN_ROOT . PHP_FOLDER . '/general/domain_paths.php'); // Константы наименований путей доменных имён
 
 // Главные файлы
 define('START_AUTH_FORM_FILEPATH', '/'); // <-> Стартовая форма авторизации Пользователя в системе
@@ -13,8 +13,8 @@ define('START_CONTENT_FILEPATH', '/content.php'); // <-> Страница кон
 define('ABOUT_FILEPATH', '/about.php'); // <-> Страница с информацией о ресурсе
 define('AUTH_FORM_HANDLING_FILEPATH', PHP_FOLDER . '/authorization/auth_form_handling.php'); // <-> Обработка данных формы авторизации Пользователя в системе
 // Вспомогательные файлы для разных страниц
-define('GET_USER_FILEPATH', PHP_FOLDER . '/auxiliary/get_user.php'); // Получение ID и email авторизованного Пользователя
-define('HTML_FRAGMENTS_FILEPATH', PHP_FOLDER . '/auxiliary/html_fragments.php'); // Различные повторяющиеся HTML-конструкции, закреплённые в php-константах
+define('GET_USER_FILEPATH', PHP_FOLDER . '/general/get_user.php'); // Получение ID и email авторизованного Пользователя
+define('HTML_FRAGMENTS_FILEPATH', PHP_FOLDER . '/general/html_fragments.php'); // Различные повторяющиеся HTML-конструкции, закреплённые в php-константах
 define('USERS_DIR', SERVER_ROOT . '/users/'); // Папка с данным Пользователей
 
 ?>
