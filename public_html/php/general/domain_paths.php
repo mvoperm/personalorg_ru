e69378a8b9a28a360c8c690f33297bc52e1a2a5d
Maksim Oskotskiy <m.oskotskiy@mail.ru>
2020-08-09 14:39:31 +0300

    Изменение наименования сайта на локальном сервере в связи с обновлением LAMP

--- public_html/php/general/domain_paths.php
+++ public_html/php/general/domain_paths.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DOMAIN_NAME', 'localhost'); // Наименование сайта 'personalorg.ru' на сервере и '/complexorg/' в OpenServer Windows
+define('DOMAIN_NAME', 'personalorg_ru'); // Наименование сайта 'personalorg.ru' на сервере и '/complexorg/' в OpenServer Windows
 define('DOMAIN_URI', 'http://' . DOMAIN_NAME); // Полный URI сайта 'https://' на сервере
 define('SERVER_ROOT', '/var/www/personalorg_ru/public_html'); // '/home/x/x926842w/x926842w.beget.tech/public_html' на сервере и '/domains/' в OpenServer Windows
 
